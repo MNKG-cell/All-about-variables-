1c3b84755aee8efa8589ee096d7839799ac1c88f
MAUREEN <maureennayebare2@gmail.com>
2020-11-09 09:29:48 +0300

    Variables part 1

---
+++ main.js
@@ -0,0 +1,7 @@
+console.log('Hello World!');
+document.getElementById("demo1"). innerHTML="0.2+0.1="+x;
+document. getElementById("demo2"). innerHTML="0.2+0.1="+y;
+document. getElementById("demo3").innerHTML=z;
+document. getElementById('demo4'). innerHTML='The result is'+ k+j;
+document. getElementById('demo5'). innerHTML=result;
+document. getElementById('demo6'). innerHTML=t;
